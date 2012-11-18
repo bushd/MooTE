4a7aef962c020b92680ca819276f3480b6ddef09
Bastian Brodbeck <bastianbrodbeck@mac.com>
2012-11-19 00:11:37 +0100

    initial commit of moote class

---
+++ moote/mt.mootools_template_engine.class.js
@@ -0,0 +1,15 @@
+var MooTE = new CLass({
+
+	Implements: [Options],
+
+	options: {
+		template: '',
+		templateURL: undefined,
+		helper: {},
+		substitude
+	},
+
+	initialize: function(options) {
+		this.setOptions(options);
+	}
+});
